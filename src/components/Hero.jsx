35c4072a24c2ff6a8e909bcd33ca272d1176a76f
Rahul Kumar Yadav <ry573870@gmail.com>
2025-03-16 20:20:02 +0530

    applied motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,38 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { motion } from "framer-motion";
-import heroImage from "../assets/hero-image.png"
+import heroImage from "../assets/hero-image.png";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const sentence = "Startup to Fortune 500";
 const words = sentence.split("");
 const Hero = () => {
   return (
-    <section id="home" className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-30 pb-6 sm:px-6 lg:px-8 ">
+    <section
+      id="home"
+      className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-30 pb-6 sm:px-6 lg:px-8 "
+    >
       {/* left column */}
       <div className="w-full md:w-1/2 space-y-8">
         {/* star badge */}
-        <div className=" flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
-          <span className="text-blue-600 group-hover:scale-110 transition-transform">
-            ⭐
-          </span>
-          <span className="text-sm font-medium">Jump start your growth</span>
-        </div>
+        <motion.div
+          variants={fadeIn("right", 0.2)}
+          initial="hidden"
+          whileInView="show"
+        >
+          <div className=" flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
+            <span className="text-blue-600 group-hover:scale-110 transition-transform">
+              ⭐
+            </span>
+            <span className="text-sm font-medium">Jump start your growth</span>
+          </div>
+        </motion.div>
 
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
+        <motion.h1 
+          variants={textVariant(0.3)}
+          initial="hidden"
+          whileInView="show"
+        className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
           We boost the growth for
           <span className="text-blue-600 relative inline-block">
             {words.map((char, index) => (
@@ -34,28 +48,47 @@ const Hero = () => {
           </span>
           Companies
           <span className="inline-block ml-2 animate-pulse">⏰</span>
-        </h1>
-        <p className="text-gray-600 text-lg md:text-xl max-w-xl">
+        </motion.h1>
+
+        <motion.p 
+          variants={fadeIn('up',0.4)}
+          initial="hidden"
+          whileInView="show"
+        className="text-gray-600 text-lg md:text-xl max-w-xl">
           Get the most accurate leads, sales people training and conversions,
           tools and more - all within the same one billing.
-        </p>
+        </motion.p>
 
-        <div className="flex gap-3 max-w-md">
+        <motion.div 
+          variants={fadeIn('up',0.5)}
+          initial="hidden"
+          whileInView="show"
+        className="flex gap-3 max-w-md">
           <input
             type="email"
             placeholder="Email address"
             className="flex-1 px-6 py-4 border border-gray-200 rounded-xl focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition-all"
           />
-          <button className="bg-blue-600 text-white px-8 py-4 rounded-xl hover:bg-blue-700 cursor-pointer transition-all hover:shadow-lg hover:shadow-blue-300">→</button>
-        </div>
+          <button className="bg-blue-600 text-white px-8 py-4 rounded-xl hover:bg-blue-700 cursor-pointer transition-all hover:shadow-lg hover:shadow-blue-300">
+            →
+          </button>
+        </motion.div>
       </div>
 
       {/* right column */}
-      <div className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12">
+      <motion.div 
+          variants={fadeIn('left',0.5)}
+          initial="hidden"
+          whileInView="show"
+      className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12">
         <div className="relative">
-            <img src={heroImage} alt="hero image" className="rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300"/>
+          <img
+            src={heroImage}
+            alt="hero image"
+            className="rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300"
+          />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,7 +15,12 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm">
+    <motion.nav 
+      variants={fadeIn('down', 0.2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once:true}}
+    className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm">
       <div className="w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 lg:h-16 h-14">
         {/* logo */}
         <div className="flex items-center gap-1 cursor-pointer">
@@ -87,7 +94,7 @@ const Navbar = () => {
           </div>
         </div>
       )}
-    </nav>
+    </motion.nav>
   );
 };
 export default Navbar;
