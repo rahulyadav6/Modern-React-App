7e97d2adb96b8c7f7ef3cda2b869d7dc2abc45c1
Rahul Kumar Yadav <ry573870@gmail.com>
2025-03-16 08:44:06 +0530

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { motion } from "framer-motion";
+import heroImage from "../assets/hero-image.png"
 
+const sentence = "Startup to Fortune 500";
+const words = sentence.split("");
 const Hero = () => {
   return (
-    <section className='container mx-auto pt-24'>Hero</section>
-  )
-}
+    <section className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-30 pb-6 sm:px-6 lg:px-8 ">
+      {/* left column */}
+      <div className="w-full md:w-1/2 space-y-8">
+        {/* star badge */}
+        <div className=" flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
+          <span className="text-blue-600 group-hover:scale-110 transition-transform">
+            ⭐
+          </span>
+          <span className="text-sm font-medium">Jump start your growth</span>
+        </div>
 
-export default Hero
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
+          We boost the growth for
+          <span className="text-blue-600 relative inline-block">
+            {words.map((char, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: index * 0.05 }}
+              >
+                {char}
+              </motion.span>
+            ))}
+          </span>
+          Companies
+          <span className="inline-block ml-2 animate-pulse">⏰</span>
+        </h1>
+        <p className="text-gray-600 text-lg md:text-xl max-w-xl">
+          Get the most accurate leads, sales people training and conversions,
+          tools and more - all within the same one billing.
+        </p>
+
+        <div className="flex gap-3 max-w-md">
+          <input
+            type="email"
+            placeholder="Email address"
+            className="flex-1 px-6 py-4 border border-gray-200 rounded-xl focus:outline-none focus:border-blue-600 focus:ring-2 focus:ring-blue-100 transition-all"
+          />
+          <button className="bg-blue-600 text-white px-8 py-4 rounded-xl hover:bg-blue-700 cursor-pointer transition-all hover:shadow-lg hover:shadow-blue-300">→</button>
+        </div>
+      </div>
+
+      {/* right column */}
+      <div className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12">
+        <div className="relative">
+            <img src={heroImage} alt="hero image" className="rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300"/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
